528389e0703e2cb4b0c1b82f4fa9338ce10e1456
hawk <80868396+acceptablehawk@users.noreply.github.com>
2022-10-17 18:03:04 -0700

    Update Puzzle_2.py

--- Puzzle_2.py
+++ Puzzle_2.py
@@ -153,14 +153,23 @@ class Puzzle:
         puzz_dict_goal = {}
 
         #-Indices which are each a key that refer to a list of neighbour indices
-        indices = {
+        if self.size == 3:
+            indices = {
             0:[1,3],        1:[0,2,4],
             2:[1,5],        3:[4,0,6],
             4:[3,5,1,7],    5:[4,2,8],
             6:[7,3],        7:[6,8,4],
             8:[7,5] 
-        }
+            }
 
+        elif self.size == 4:
+            indices = {
+            0:[1,4], 1:[0,5,2], 2:[1,6,3], 3:[2,7],
+            4:[0,5,8], 5:[1,4,9,6], 6:[2,5,7,10], 7:[3,6,11],
+            8:[4,9,12], 9:[5,8,10,13], 10:[6,9,11,14], 11:[7,10,15],
+            12:[8,13], 13:[9,12,14], 14:[10,13,15], 15:[11,14]
+        }
+           
         for i in range(0,len(start)):
             for j in range(0,len(start[i])):
                 puzz_dict_start[start[i][j]] = [i,j]
